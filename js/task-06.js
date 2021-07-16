894051e8d9966c99035a0fcedc178efecf86453e
Pasha <pashahrytsyna@gmail.com>
2021-07-16 21:50:38 +0300

    change task 06

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,10 @@
 let total = 0,
     input = prompt('Введите число', 0)
 
-for(let i = 0; ; i += 1) {
-    if(input != null) {
+for(let i = 0; input != null; i += 1) {
+        if(Number(input) )
         total += Number(input)
         input = prompt('Введите число', 0)
-    } else {
-        break
-    }
 }
 
 alert(`Общая сумма чисел равна ${total}`)
-
-
