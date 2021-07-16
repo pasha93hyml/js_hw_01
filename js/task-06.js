fe29e0892272184c166f1b262fa796292d707f30
Pasha <pashahrytsyna@gmail.com>
2021-07-16 21:52:14 +0300

    change task 06

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,6 @@ let total = 0,
     input = prompt('Введите число', 0)
 
 for(let i = 0; input != null; i += 1) {
-        if(Number(input) )
         total += Number(input)
         input = prompt('Введите число', 0)
 }
