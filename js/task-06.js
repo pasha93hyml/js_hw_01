81782ef100821a7e383756bf6d5baf65d22a2dd5
Pasha <pashahrytsyna@gmail.com>
2021-07-17 13:50:59 +0300

    add verify isNaN. task 06

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,14 @@ let total = 0,
     input = prompt('Введите число', 0)
 
 for(let i = 0; input != null; i += 1) {
+    if(!isNaN(Number(input))) {
         total += Number(input)
         input = prompt('Введите число', 0)
+    } else {
+        input = prompt('Введите число', 0)
+    }
 }
 
 alert(`Общая сумма чисел равна ${total}`)
+
+
